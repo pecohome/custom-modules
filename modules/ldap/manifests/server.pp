1507de7afe12c7b577a8d2f3696cd42065b66d4b
Prasanna Kamalakar <prasanna@Prasannas-MacBook-Pro.local>
2025-01-31 10:26:10 -0500

    first commit

---
+++ modules/ldap/manifests/server.pp
@@ -0,0 +1,29 @@
+class ldap::server {
+  # Ensure the necessary package is installed
+  package { 'slapd':
+    ensure => installed,
+  }
+
+  # present the configuration to avoid the interactive prompts during installation
+  exec { 'pressed-slapd':
+    command => '/usr/bin/debconf-set-selections <<< "slapd slapd/no_configuration boolean false"',
+    unless  => '/usr/bin/dpkg-query -W slapd',
+    before  => Package['slapd'],
+  }
+
+  # Ensure that the ldap service is running and enabled
+  service { 'slapd':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+  }
+
+  # Add a basic configuration file if needed
+  file { '/etc/openldap/ldap.conf':
+    ensure  => file,
+    content => template('ldap/ldap.conf.erb'),
+    require => Package['slapd'],
+    notify  => Service['slapd'],
+  }
+
+}
