6a5886f03681b52e80c9ef1f6ad6705f27de4f46
Prasanna Kamalakar <prasanna@Prasannas-MacBook-Pro.local>
2025-01-31 18:15:48 -0500

    keep trying

--- manifests/openldap.pp
+++ manifests/openldap.pp
@@ -1,4 +1,4 @@
-class openldap::server {
+class { 'openldap::server':
   ensure         => present,
   root_dn        => 'cn=admin,dc=home,dc=lab',
   root_password  => lookup('openldap::root_password'),
@@ -7,13 +7,13 @@ class openldap::server {
     'to attrs=userPassword by self write by anonymous auth by dn="cn=admin,dc=home,dc=lab" write by * none',
     'to * by self write by dn="cn=admin,dc=home,dc=lab" write by * read',
   ],
+}
 
-  # Ensure OpenLDAP service is running
-  service { 'slapd':
-    ensure  => running,
-    enable  => true,
-    require => Class['openldap::server'],
-  }
+# Ensure OpenLDAP service is running
+service { 'slapd':
+  ensure  => running,
+  enable  => true,
+  require => Class['openldap::server'],
 }
 
 # Create an organizational unit
